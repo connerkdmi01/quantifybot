931ab14c430dbe80558bda9256d54dcb51cff59c
Conner Mi <connerkdmi01@gmail.com>
2021-06-22 21:32:06 -0500

    giselle is dumb

--- main.py
+++ main.py
@@ -13,14 +13,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    content = message.content
+
+    replacedict = {
+        "a": "∀",
+        "e": "∃"
+    }
+
     if message.author == client.user:
+        if "I totally agree! He's super dumb." in content:
+            await message.channel.send("But Giselle's even dumber!")
         return
 
-    if message.content.startswith('$hello'):
+    if content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+    if content.startswith('!q'):
+        msg = ''
+        for e in content:
+            if e.lower() in replacedict.values():
+                msg += dict.get(e.lower())
+            else:
+                msg += e
+        await message.channel.send(msg)
     
-    if "Conner is dumb" in message.content:
+    if "Conner is dumb" in content:
         await message.channel.send("I totally agree! He's super dumb.")
+
 
 token = os.getenv('TOKEN')
 client.run(token)
