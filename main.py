1bff5cbf2b94d4ece7c606e49b13267bae406a02
Feardra <71064090+Feardra@users.noreply.github.com>
2021-06-22 17:39:31 -0700

    poggers

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import discord
 import os
+from dotenv import load_dotenv
 
+load_dot_env()
+TOKEN = os.getenv("TOKEN")
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Poggers it is me, {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
